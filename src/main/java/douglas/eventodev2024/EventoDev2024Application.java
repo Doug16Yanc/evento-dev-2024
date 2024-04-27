dbddb71c61a2bcf95d3f9fb8686eec76e891dc84
douglas <douglasholanda3195@gmail.com>
2024-04-27 14:59:21 -0400

    Initial commit

---
+++ src/main/java/douglas/eventodev2024/EventoDev2024Application.java
@@ -0,0 +1,13 @@
+package douglas.eventodev2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventoDev2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventoDev2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/douglas/eventodev2024/EventoDev2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package douglas.eventodev2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventoDev2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
